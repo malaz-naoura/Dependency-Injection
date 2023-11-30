4f0f6f3ca8679b088c28d6d96d1d44ef38f6a752
malaz_naoura_rog <malaznaoura@gmail.com>
2023-11-30 12:13:34 +0300

    init project

---
+++ src/test/java/com/mezo/trainin/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mezo.trainin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
