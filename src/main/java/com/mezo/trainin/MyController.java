0143da668fb1a3738750db7a884f54da6d149cf3
malaz_naoura_rog <malaznaoura@gmail.com>
2023-11-30 13:25:00 +0300

    4- spring Context

---
+++ src/main/java/com/mezo/trainin/MyController.java
@@ -0,0 +1,12 @@
+package com.mezo.trainin;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void sayHello(){
+        System.out.println("hello folks");
+    }
+
+}
--- src/main/java/com/mezo/trainin/TrainingApplication.java
+++ src/main/java/com/mezo/trainin/TrainingApplication.java
@@ -2,12 +2,17 @@ package com.mezo.trainin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TrainingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TrainingApplication.class, args);
+
+        ApplicationContext ctx= SpringApplication.run(TrainingApplication.class, args);
+        MyController greeting= (MyController) ctx.getBean("myController");
+
+        greeting.sayHello();
     }
 
 }
