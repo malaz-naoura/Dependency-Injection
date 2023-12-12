1900ec67bcb2a83f73e2df705e388ffcb85061cf
malaz_naoura <malaznaoura@gmail.com>
2023-12-12 14:19:32 +0300

    Get username and password from environment variables instead of properties file

--- src/main/java/com/mezo/trainin/config/propertyConfig.java
+++ src/main/java/com/mezo/trainin/config/propertyConfig.java
@@ -1,29 +1,27 @@
 package com.mezo.trainin.config;
 
 import com.mezo.trainin.dataSource.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class propertyConfig {
 
-    @Value("${mezo.username}")
-    private String username;
-
-    @Value("${mezo.password}")
-    private String password;
+    @Autowired
+    Environment env;
 
     @Value("${mezo.url}")
     private String url;
 
     @Bean
     FakeDataSource getDatasource(){
-        System.out.println(username+" "+password+" "+url);
-        return new FakeDataSource(username,password,url);
+        return new FakeDataSource(env.getProperty("MEZO_USERNAME"),env.getProperty("MEZO_PASS"),url);
     }
 
 
